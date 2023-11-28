f7ec1a72ed43706d192c0430a8cc37a232e37d53
vvekro <vivek.mahesh.rao@gmail.com>
2023-11-28 13:12:32 +0530

    qr reader in main

---
+++ main.py
@@ -0,0 +1,38 @@
+'''
+all imports go here, group according to module
+'''
+
+# general
+import sys      # used to import folders containing classfiles
+sys.path.insert(0, 'flight/')
+sys.path.insert(0, 'network/')
+sys.path.insert(0, 'tools/')
+import threading
+
+# camera
+from tools.QrReader import QrReader
+
+
+
+'''
+All function defnintions, group according to module
+'''
+
+# camera
+def camera_runner(camera_output):
+    ''' To be placed in a thread with camera_output list as arg, continu'''
+    camera = QrReader()
+    while True:
+        result = camera.classify()
+        if result is not None:
+            camera_output.append(result)
+
+
+
+
+def main():
+    print("still empty")
+
+
+if __name__ =="__main__":
+    main()
