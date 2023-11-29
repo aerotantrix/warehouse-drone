c6949c0133165662c3fee15dbec69ff4b3acd8f8
DEVSamurai <93643661+COD-SAMURAI@users.noreply.github.com>
2023-11-29 20:26:55 +0530

    main update2.0

--- main.py
+++ main.py
@@ -12,7 +12,9 @@ from multiprocessing import Process
 # camera
 from tools.QrReader import QrReader
 import cv2
-
+ 
+#Queue
+import queue
 
 
 '''
@@ -20,10 +22,10 @@ All function defnintions, group according to module
 '''
 
 # camera
-def camera_runner(camera_output, cap):
+def camera_runner(camera_output):
     ''' To be placed in a thread with camera_output list as arg
-        will always be running and writes non empty results into the list
-        REMEMBER TO CLEAR THE LIST
+        will always be running and writes non empty results into the queue
+        REMEMBER TO CLEAR THE QUEUE
     '''
     camera = QrReader()     # creates QrReader object
     cap = cv2.VideoCapture(0)   # calls VideoCapture and stores image in cap
@@ -32,17 +34,20 @@ def camera_runner(camera_output, cap):
         if success:     # calls this only if image is retrieved
             result = camera.classify(img)   # calls classify passing the array as arg
             if result is not None:
-                camera_output.append(result)    # writes to the camera_output list to be processed later
+                camera_output.put(result)    # writes to the camera_output queue to be processed later
 
 
-
+#main
 
 def main():
     print("still empty")
     '''
     Processes for each feature have to be created and called here
     '''
-
+    camera_output = queue.Queue()
+    camera_process = Process(target=camera_runner, args=(camera_output,))
+    camera_process.start()
+    print(camera_output.get())
 
 if __name__ =="__main__":
     main()
